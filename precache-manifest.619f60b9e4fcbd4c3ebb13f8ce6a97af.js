4a0c758820cded39230d440cd6cfe0aa36ad266b
haohao <a83811123@gmail.com>
2019-07-25 17:30:05 +0800

    Updates

---
+++ precache-manifest.619f60b9e4fcbd4c3ebb13f8ce6a97af.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb385f15a3701034f4247910544fd7f",
+    "url": "/HaosTea/index.html"
+  },
+  {
+    "revision": "edece9088ae10cb9e1ed",
+    "url": "/HaosTea/static/css/main.0a6860c9.chunk.css"
+  },
+  {
+    "revision": "e308d22c9181ccb70490",
+    "url": "/HaosTea/static/js/2.28aa4949.chunk.js"
+  },
+  {
+    "revision": "edece9088ae10cb9e1ed",
+    "url": "/HaosTea/static/js/main.79a113b2.chunk.js"
+  },
+  {
+    "revision": "0a723f6abb48d7178b08",
+    "url": "/HaosTea/static/js/runtime~main.91076bc0.js"
+  },
+  {
+    "revision": "b845c8c7b9e3a59a346124eff0ab96b9",
+    "url": "/HaosTea/static/media/2017winter.b845c8c7.png"
+  },
+  {
+    "revision": "2359408a22116ba266bba6db69d90523",
+    "url": "/HaosTea/static/media/2018spring.2359408a.jpg"
+  },
+  {
+    "revision": "578dbb988ffa8f84fee5c231509e1215",
+    "url": "/HaosTea/static/media/EnglishBreakfast.578dbb98.jpg"
+  },
+  {
+    "revision": "800f5f6aff2c444a2eb44720b3d625d8",
+    "url": "/HaosTea/static/media/FormosaOolong.800f5f6a.jpg"
+  },
+  {
+    "revision": "52d7c7285198122301cb79c859f3c6d5",
+    "url": "/HaosTea/static/media/MoroccanMint.52d7c728.png"
+  },
+  {
+    "revision": "8b54d73f1731b601adbd5de70310c0f4",
+    "url": "/HaosTea/static/media/Oolongteabox.8b54d73f.png"
+  },
+  {
+    "revision": "25285581778732eed54975b4b4a9e679",
+    "url": "/HaosTea/static/media/TypeLand-com-KangXiZiDianTi-ShiYongBan-1.25285581.otf"
+  },
+  {
+    "revision": "be53cb6b1298a92f7e39be6b7b42a28a",
+    "url": "/HaosTea/static/media/about2.be53cb6b.jpg"
+  },
+  {
+    "revision": "b918929eb3a562ef5d673dea070a951a",
+    "url": "/HaosTea/static/media/about3.b918929e.jpg"
+  },
+  {
+    "revision": "351d2957722f550ce5a77d3f138cedd1",
+    "url": "/HaosTea/static/media/about_bg.351d2957.jpg"
+  },
+  {
+    "revision": "27cf40b452e4a11f31985361720c2625",
+    "url": "/HaosTea/static/media/aboutbgleft.27cf40b4.png"
+  },
+  {
+    "revision": "872c8c9f926f0b3526d3b3afd59f1815",
+    "url": "/HaosTea/static/media/aboutbgright.872c8c9f.png"
+  },
+  {
+    "revision": "f67a49634d3cf8e68e08c90e938d3218",
+    "url": "/HaosTea/static/media/abouttea1.f67a4963.jpg"
+  },
+  {
+    "revision": "54f33f73e2db08a69c8e17b27cecf4c1",
+    "url": "/HaosTea/static/media/abouttea2.54f33f73.jpg"
+  },
+  {
+    "revision": "300dfd405934b72d7bccd850c3ad7ac5",
+    "url": "/HaosTea/static/media/abouttea3.300dfd40.jpg"
+  },
+  {
+    "revision": "08cccce28d90ef73b1669e8b32de1ae2",
+    "url": "/HaosTea/static/media/blackteabox.08cccce2.png"
+  },
+  {
+    "revision": "93eb64608320729f4d9260dcd81c6901",
+    "url": "/HaosTea/static/media/contact_bg.93eb6460.jpg"
+  },
+  {
+    "revision": "409b8dd8791e81ff62655054ceceb908",
+    "url": "/HaosTea/static/media/footer.409b8dd8.png"
+  },
+  {
+    "revision": "5d5ccdb23015fdba6109dbde6ce0f8e3",
+    "url": "/HaosTea/static/media/greenteabox.5d5ccdb2.png"
+  },
+  {
+    "revision": "3fffa3f23448a8e7e04cda0dbd582639",
+    "url": "/HaosTea/static/media/homepage_banner.3fffa3f2.jpg"
+  },
+  {
+    "revision": "d87d94bb47628e70eb5f4da3be7faf77",
+    "url": "/HaosTea/static/media/homepage_left_bg.d87d94bb.png"
+  },
+  {
+    "revision": "c6aec45b0298c808485bd3408b451ebe",
+    "url": "/HaosTea/static/media/homepage_vider_bg.c6aec45b.png"
+  },
+  {
+    "revision": "71fe1da0d4ae1d95a9fdbd334154e78e",
+    "url": "/HaosTea/static/media/homepagevideo.71fe1da0.mp4"
+  },
+  {
+    "revision": "ea89f61ba90dc6986ed919f39017a1c1",
+    "url": "/HaosTea/static/media/img2.ea89f61b.jpg"
+  },
+  {
+    "revision": "08b12bc1324b9d23ffe29c62821981f8",
+    "url": "/HaosTea/static/media/img3.08b12bc1.jpg"
+  },
+  {
+    "revision": "12467bbb5d1778691d132a14bcc4f4e2",
+    "url": "/HaosTea/static/media/map.12467bbb.jpg"
+  },
+  {
+    "revision": "33d0a13207316cd583cc976811c6f03e",
+    "url": "/HaosTea/static/media/news1.33d0a132.jpg"
+  },
+  {
+    "revision": "d80b3d098c1f966225e2102b37c562bc",
+    "url": "/HaosTea/static/media/news2.d80b3d09.jpg"
+  },
+  {
+    "revision": "b6dda2711d038a9f778c595a668fd17d",
+    "url": "/HaosTea/static/media/product_bg.b6dda271.jpg"
+  },
+  {
+    "revision": "9967ba9d3a0557dcb70ded7a59cd6234",
+    "url": "/HaosTea/static/media/store1.9967ba9d.jpg"
+  },
+  {
+    "revision": "d952fa778c3fd26307e39a2d4ea7c3f5",
+    "url": "/HaosTea/static/media/teabag.d952fa77.jpg"
+  }
+]);
